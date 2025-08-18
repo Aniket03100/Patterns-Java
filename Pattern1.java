8827f20eb0d1ebdfbe64d926e6b7e94948aa6443
Aniket Bhiungade <aniketbhiungade@gmail.com>
2025-08-18 21:23:38 +0530

    Added Pattern1

---
+++ Pattern1.java
@@ -0,0 +1,31 @@
+// OUTPUT
+// * * * * 
+// * * * * 
+// * * * * 
+// * * * *
+
+
+import java.util.Scanner;
+
+public class Pattern1 {
+
+    void square(int n){
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                System.out.print("* ");
+            }
+             System.out.println();
+        }
+       
+        
+
+    }
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Pattern1 obj = new Pattern1();
+        obj.square(n);
+
+    }
+}
