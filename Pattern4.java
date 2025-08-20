ad5f39047542066e7267c01645b9ff5f7d4ee3ee
Aniket Bhiungade <aniketbhiungade@gmail.com>
2025-08-20 13:13:29 +0530

    Added Pattern4

---
+++ Pattern4.java
@@ -0,0 +1,27 @@
+// OUTPUT
+// 1 
+// 2 2 
+// 3 3 3
+// 4 4 4 4
+// 5 5 5 5 5
+
+
+import java.util.Scanner;
+
+public class Pattern4 {
+    static void rightAngledTriNum2(int n){
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        rightAngledTriNum2(n);
+
+    }
+}
